4f60bcf53ae3c901dac447cf875a94a0bb37771c
Eptesicus <dieter.vanderelst@fastmail.net>
2024-07-05 15:36:48 -0400

    add single position measurement script

--- SCRIPT_TestMove.py
+++ SCRIPT_TestMove.py
@@ -4,10 +4,10 @@ from R12 import RobotBat
 
 R = RobotBat.RobotBat(connect_robot=True, connect_sonar=False)
 
-world_x = -700
-world_y = 150
+world_x = -200
+world_y = 200
 world_pitch = 0
-world_yaw = 0
+world_yaw = 27.5
 
 world_z = numpy.interp(world_y, [0, 100, 200, 300], [500, 400, 300, 300])
 R.set_position(world_x, world_y, world_z, world_yaw, world_pitch)
