3134e34573a87d07121b4cab219b3c149eb7fc43
Thinh Nguyen <huythinhnguyen@gmail.com>
2024-07-10 12:48:59 -0400

    MeasureSinglePosition Ready

---
+++ MeasureSinglePostion_Run.py
@@ -0,0 +1,60 @@
+import sys
+import os
+import logging
+import datetime
+import time
+
+
+DATE = datetime.datetime.now().strftime('%Y-%m-%d')
+TIME = datetime.datetime.now().strftime('%H-%M-%S')
+SAVE_PATH = os.path.join('data', DATE, TIME)
+LOG_FILE = os.path.join(SAVE_PATH, 'log.txt')
+# Path to the directory where the script is located
+if not os.path.exists(SAVE_PATH): os.makedirs(SAVE_PATH)
+logging.basicConfig(filename=LOG_FILE, level=logging.INFO, format='%(asctime)s %(message)s')
+from script import SCRIPT_MeasureSinglePosition as script
+#####################################################
+######### USER EDIT FROM HERE #######################
+
+WORLD_X = -200 #mm 
+WORLD_Y = 200 #mm
+WORLD_PITCH = 0 #deg
+WORLD_YAW = 27.5 #deg
+DRY_RUN = False # Set False to do measurement, True to skip measurement
+REPEATS = 50
+RT_PLOT = False # Set True to plot the data in real time
+DELAY = 0.25
+SAVE_DATA = False # Set True to save the data
+REACH_TABLE = {
+    'X': None,
+    'Y': [0, 100, 200, 300],
+    'Z': [500, 400, 300, 300],
+} # This is need to revise to a function of a circle --> Not optimal but leave it here for now
+DESCRIPTION = ''
+
+
+######### USER EDIT UNTIL HERE #######################
+######################################################
+
+def main():
+    return script.do_measurement(WORLD_X, WORLD_Y, WORLD_PITCH, WORLD_YAW,
+                                 REPEATS, DRY_RUN, RT_PLOT, DELAY, SAVE_DATA,
+                                 REACH_TABLE, DESCRIPTION, SAVE_PATH)
+
+    logging.info("WORLD_X = %s", WORLD_X)
+    logging.info("WORLD_Y = %s", WORLD_Y)
+    logging.info("WORLD_PITCH = %s", WORLD_PITCH)
+    logging.info("WORLD_YAW = %s", WORLD_YAW)
+    logging.info("DRY_RUN = %s", DRY_RUN)
+    logging.info("REPEATS = %s", REPEATS)
+    logging.info("RT_PLOT = %s", RT_PLOT)
+    logging.info("DELAY = %s", DELAY)
+    logging.info("SAVE_DATA = %s", SAVE_DATA)
+    logging.info("REACH_TABLE = %s", REACH_TABLE)
+    logging.info("DESCRIPTION = %s", DESCRIPTION)
+    logging.info("SAVE_PATH = %s", SAVE_PATH)
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ script/SCRIPT_MeasureSinglePosition.py
@@ -0,0 +1,64 @@
+import time
+import numpy as np  
+import os
+import sys
+import pickle
+from R12 import RobotBat
+# from R12 import CombinationTools
+from matplotlib import pyplot as plt
+
+
+world_x = -200
+world_y = 200
+world_pitch = 0
+world_yaw = 27.5
+repeats = 50
+reach_table = {'x': None, 'y': [0, 100, 200, 300], 'z': [500, 400, 300, 300]}
+description = 'Test'
+
+
+def do_measurement(world_x:int,
+                   world_y:int,
+                   world_pitch:int,
+                   world_yaw:int,
+                   repeats:int,
+                   dry_run:bool,
+                   plot:bool,
+                   delay:float,
+                   save_data:bool,
+                   reach_table:dict,
+                   description:str, save_path:str='../data/test'):
+    R = RobotBat.RobotBat(connect_robot=True, connect_sonar=(not dry_run))
+    world_z = np.interp(world_y, reach_table['y'], reach_table['z'])
+    R.set_position(world_x, world_y, world_z, world_yaw, world_pitch)
+    if not dry_run:
+        data =  R.measure(subtract_floor=True, plot=plot)
+        data_array = np.zeros((repeats, data.shape[0], data.shape[1]))
+        file_name = os.path.join(save_path, 'data.pkl')
+        full_file_name = os.path.join('data', file_name + '.pkl')
+        for i in range(repeats):
+            print('###########', i)
+            data = R.measure(plot=False, subtract_floor=False)
+            data_array[i,:,:] = data
+            time.sleep(delay)
+    if not dry_run:
+        data_to_save = {
+            'data_array': data_array,
+            'world_x': world_x,
+            'world_y': world_y,
+            'world_z': world_z,
+            'world_pitch': world_pitch,
+            'world_yaw': world_yaw,
+            'repeats': repeats,
+            'description': description
+        }
+
+        file = open(full_file_name, 'wb')
+        pickle.dump(data_to_save, file)
+        file.close()
+        if plot:
+            plt.plot(np.mean(data_array, axis=0))
+            plt.show()
+
+if __name__=='__main__':
+    do_measurement(world_x, world_y, world_pitch, world_yaw, repeats, False, False, 0.25, False, reach_table, description)
